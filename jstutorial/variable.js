bac7222326c0d2730794d38b66e489eda25adcb7
Dileep Kumar <162305591+Dileepkr01@users.noreply.github.com>
2025-05-26 14:09:13 +0000

    some variable

---
+++ jstutorial/variable.js
@@ -0,0 +1,15 @@
+const AccountId = 1234
+let AccoountEmail = "Dileep@google.com"
+var AccoountPassword = "36236"
+AccoountCity = "jaipur"
+
+console.table([AccountId,AccoountEmail,AccoountPassword,AccoountCity]);
+
+// AccountId = 2
+ AccoountEmail ="Dk@dm.com"
+ AccoountPassword = "5678"
+ AccoountCity = "Delhi"
+ let AccoountName;
+console.table([AccountId,AccoountEmail,AccoountPassword,AccoountCity,AccoountName]);
+
+
