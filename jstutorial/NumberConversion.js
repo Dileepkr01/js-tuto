98b71563de0575965376276291005978d4a7168e
Dileep Kumar <162305591+Dileepkr01@users.noreply.github.com>
2025-05-26 14:36:27 +0000

    Number conversion

---
+++ jstutorial/NumberConversion.js
@@ -0,0 +1,17 @@
+// let score = "33abc"
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+// let valInNumber = Number(score)
+// console.log(valInNumber);
+
+
+let x = 100
+let y = Number(x)
+console.log(typeof x)
+let z = Boolean(x)
+console.log(typeof z);
+
+console.log(z)
+
